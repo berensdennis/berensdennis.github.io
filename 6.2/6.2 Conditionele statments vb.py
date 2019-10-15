ef2a2fcebcf9805f860cacffba8b2c31b162eb69
berensdennis <56117546+berensdennis@users.noreply.github.com>
2019-10-15 12:51:36 +0200

    oefeningen hoofdstuk 6.1 tweede poing

---
+++ 6.2/6.2 Conditionele statments vb.py
@@ -0,0 +1,9 @@
+x=5
+if x==5:
+    print("x is 5")
+
+x=7
+if x<10:
+    print("Deze regel wordt alleen uitgevoerd als x<10.")
+    print("En dat geldt ook voor deze regel.")
+print("Deze regel wordt altijd uitgevoerd")
---
+++ 6.2/6.2 meer-weg beslissingen.py
@@ -0,0 +1,10 @@
+leeftijd = 21
+if leeftijd < 12:
+    print("je bent een kind")
+elif leeftijd <18:
+    print("je bent een teenager")
+elif leeftijd <30:
+    print("je bent nog jong")
+elif leeftijd <50:
+    print("Beginnen grijze haren te komen?")
+else: print("wegen de jaren zwaar?")
